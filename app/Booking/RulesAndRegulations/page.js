3f93039d9ebce8a645f583f9badfc5afca01117e
hphalke0802 <147521768+hphalke0802@users.noreply.github.com>
2026-09-01 13:06:41 +0530

    Delete app directory

--- app/Booking/RulesAndRegulations/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RulesAndRegulations = () => {
-  return (
-    <div className='flex items-center justify-center text-3xl font-bold min-h-screen'>Rules And Regulations </div>
-  )
-}
-
-export default RulesAndRegulations
